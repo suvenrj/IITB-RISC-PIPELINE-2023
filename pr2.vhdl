135febd516e1c5f87a0e9483361ec8a7c9262da3
sankethan <rsankethan9rathod@gmail.com>
2023-04-30 23:51:37 +0530

    rectified syntax errors

--- pr2.vhdl
+++ pr2.vhdl
@@ -6,10 +6,12 @@ entity pr2 is
          pc_next: in std_logic_vector(15 downto 0);				-- if Instruction at PC is at ID stage then PC_next is PC+2
 			pr2_wr_en: in std_logic;
 			clk: in std_logic;
+			enable : in std_logic;
 			pr2_out: out std_logic_vector(55 downto 0)); 
+
 end entity;
 
-architecture behave of Decoder is 
+architecture behave of pr2 is 
 
 signal reg_sig : std_logic_vector(55 downto 0);  -- sign_ext control
 
