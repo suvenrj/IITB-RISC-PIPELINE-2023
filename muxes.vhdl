818e9dd87b0508f56d5bc0de7a8e18eecb22acb7
sankethan <rsankethan9rathod@gmail.com>
2023-05-01 04:41:15 +0530

    Added 4x1 mux

--- muxes.vhdl
+++ muxes.vhdl
@@ -1,20 +1,48 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity mux2x1 is
-	port(I1: in std_logic_vector(2 downto 0);
-	     I2: in std_logic_vector(2 downto 0);
+	port(I0: in std_logic_vector(2 downto 0);
+	     I1: in std_logic_vector(2 downto 0);
 		 S0: in std_logic;
 		I_out:out std_logic_vector(2 downto 0));
 end entity;
 
 architecture behavioral of mux2x1 is
 	begin
-		process (S0, I1, I2)
+		process (S0, I0, I1)
 		begin
 			if S0 = '0' then
-				I_out <= I1;
+				I_out <= I0;
 			else
-				I_out <= I2;
+				I_out <= I1;
 			end if;
 		end process;
 end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+entity mux4x1 is
+    Port ( I00 : in  STD_LOGIC_VECTOR (2 downto 0);
+			I01 : in  STD_LOGIC_VECTOR (2 downto 0);
+			I10: in  STD_LOGIC_VECTOR (2 downto 0);
+			I11:in  STD_LOGIC_VECTOR (2 downto 0);
+           S : in  STD_LOGIC_VECTOR (1 downto 0);
+           I_out : out  STD_LOGIC_VECTOR(2 downto 0));
+end mux4x1;
+
+architecture Behavioral of mux4x1 is
+begin
+    process (I00,I01,I10,I11, S)
+    begin
+        case S is
+            when "00" =>
+                I_out <= I00;
+            when "01" =>
+			I_out <= I01;
+            when "10" =>
+			I_out <= I10;
+            when others =>
+                I_out <= I00;
+        end case;
+    end process;
+end Behavioral;
