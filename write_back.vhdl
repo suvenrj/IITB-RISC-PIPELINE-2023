5065f85c2206d9f0a34e675508353abf648a92c5
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-01 19:51:32 +0530

    removed pc_next

--- write_back.vhdl
+++ write_back.vhdl
@@ -2,25 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity write_back is
-    port(data_in, pc_next: in std_logic_vector(15 downto 0); 
-    dest_reg_in: in std_logic_vector(2 downto 0); 
-    reg_write_en_in: in std_logic; 
+    port( 
     reg_write_en_out: out std_logic;
     data_out: out std_logic_vector(15 downto 0); 
     dest_reg_out: out std_logic_vector(2 downto 0); 
-    opcode: in std_logic_vector(3 downto 0));
+    pr5_out : in std_logic_vector(19 downto 0); 
+);
 end entity;
 
 architecture arch of write_back is
 begin
-    dest_reg_out <= dest_reg_in;
-    reg_write_en_out <= reg_write_en_in;
-    proc1: process (data_in, pc_next) 
-    begin
-        if (opcode = "1100" or opcode = "1101") then  -- JAL, JLR
-            data_out <= pc_next;
-        else
-            data_out <= data_in;
-        end if;
-    end process;
+    dest_reg_out <= pr5_out(18 downto 16);
+    reg_write_en_out <= pr5_out(19);
+    data_out <= pr5_out(15 downto 0);
 end arch ; -- arch
