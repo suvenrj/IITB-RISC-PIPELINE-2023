7ffa526b3bc44342e9c48276debe580ee45ab4d7
JayGC <96428509+JayGC@users.noreply.github.com>
2023-05-01 14:59:03 +0530

    Update execution (1).vhd

--- execution (1).vhd
+++ execution (1).vhd
@@ -2,41 +2,50 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity exec is
-	port(clock,c_in,z_in,write_en_in: in std_logic;instruction_in,data1,data2,data_imm: in std_logic_vector(15 downto 0);z_out,c_out,write_en_out: out std_logic;instruction_out,data_imm_out,data_out,data_out1: out std_logic_vector(15 downto 0));
+	port(clock,c_in,z_in,write_en_in,m6_control: in std_logic;m1_control:in std_logic_vector(1 downto 0);alu_control:in std_logic_vector(2 downto 0);data1,data2,data_imm: in std_logic_vector(15 downto 0);z_out,c_out,write_en_out: out std_logic;data_imm_out,data_out,data_out1: out std_logic_vector(15 downto 0));
 end entity exec;
 
 -- data 1 and data 2 are for ALU but data_imm is needed for beq instr
 
 architecture bhv of exec is
     signal control_bits : std_logic_vector(3 downto 0);
+    signal data_2c : std_logic_vector(15 downto 0);
 
-    component ALU is
-        port(ALU_A,ALU_B: in std_logic_vector(15 downto 0);control: in std_logic_vector(2 downto 0); c_in: in std_logic;
-            c_out,zero: out std_logic;ALU_C: out std_logic_vector(15 downto 0));
-    end component ALU;
+    component basic_ALU is
+	    port(ALU_A, ALU_B: in std_logic_vector(15 downto 0);Carry_prev: in std_logic;Control: in std_logic_vector(2 downto 0);ALU_C: out std_logic_vector(15 downto 0);Carry_new,Zero_new : out std_logic);
+    end component;
+
+    component complimenter is
+        port ( Inp : in  std_logic_vector(15 downto 0);Outp : out std_logic_vector(15 downto 0));
+    end component;
 
     begin
-		control_bits(2 downto 0) <= instruction_in(2 downto 0);
-        control_bits(3) <= instruction_in(15);
-        instruction_out <= instruction_in;
+		control_bits(2 downto 0) <= alu_control;
+        control_bits(3) <= m6_control;
         data_imm_out <= data_imm;
         data_out1 <= data1;
-        ALU1: ALU port map(data1,data2,control_bits,c_in,c_out,z_out,data_out);
+        CPL : complimenter port map(data2,data_2c);
 
-        proc1: process(clock,c_in,z_in,write_en_in)
+        proc0: process(data_2c)
         begin
-            if(instruction_in(15 downto 12) = "0001" | instruction_in(15 downto 12) = "0010" ) then
-                case control_bits(1 downto 0) is
+            if(control_bits(3)='1') then
+                data2 <= data_2c;
+            else
+                data2 <= data2;
+            end if;
+        end process;
+        
+        ALU1: basic_ALU port map(data1,c_in,control_bits,data_out,c_out,z_out);
+
+        proc1: process(clock,c_in,z_in,write_en_in) --  i think there is no need of process here
+                case m1_control(1 downto 0) is
                     when "10" =>
                         write_en_out <= c_in;
-                    when "01" =>
+                    when "11" =>
                         write_en_out <= z_in;
                     when others =>
                         write_en_out <= write_en_in;
                 end case;
-            else
-                write_en_out <= write_en_in;
-            end if;
         end process;
 
 end bhv;
