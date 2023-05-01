b8c5760419d5738f4e491dbb6574613035bc007a
sankethan <rsankethan9rathod@gmail.com>
2023-05-01 17:05:38 +0530

    ADDED new muxes and edited pr3

--- muxes.vhdl
+++ muxes.vhdl
@@ -67,3 +67,47 @@ architecture behavior of mux2x1_1bit is
 		end process;
 end architecture;
 
+library ieee;
+use ieee.std_logic_1164.all;
+entity mux4x1_1bit is
+	port(I00,I01,I10,I11: in std_logic;
+		 S0: in std_logic_vector(1 downto 0);
+		I_out:out std_logic);
+end entity;
+
+architecture behavior of mux4x1_1bit is
+	begin
+		process (S0, I00, I01,I10,I11)
+		begin
+			if S0 = "00" then
+				I_out <= I00;
+			elsif s0 = "01" then
+				I_out <= I01;
+			elsif s0 = "10" then
+				I_out <= I10;
+			else
+				I_out <= I11;
+			end if;
+		end process;
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+entity mux2x1_16bit is
+	port(I0: in std_logic_vector(15 downto 0);
+	     I1: in std_logic_vector(15 downto 0);
+		 S0: in std_logic;
+		I_out:out std_logic_vector(15 downto 0));
+end entity;
+
+architecture behavioral of mux2x1_16bit is
+	begin
+		process (S0, I0, I1)
+		begin
+			if S0 = '0' then
+				I_out <= I0;
+			else
+				I_out <= I1;
+			end if;
+		end process;
+end architecture;
--- pr3.vhdl
+++ pr3.vhdl
@@ -2,52 +2,54 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity pr3 is
-    port(opr1 : in std_logic_vector(15 downto 0);
-        opr2: in std_logic_vector(15 downto 0);
-        imm : in std_logic_vector(15 downto 0);
+    port(opr1 : in std_logic_vector(15 downto 0); --
+        opr2: in std_logic_vector(15 downto 0); --
+        imm : in std_logic_vector(15 downto 0); --
         pc_next: in std_logic_vector(15 downto 0);	-- if Instruction at PC is at ID stage then PC_next is PC+2
-        reg_a_data_sw: in std_logic_vector(15 downto 0);	
-        dest_reg: in std_logic_vector(2 downto 0);
-        alu_control: in std_logic_vector(2 downto 0);
-        carry_write_en: in std_logic;
-        zero_flag_en: in std_logic;
-        m5_control: in std_logic_vector(1 downto 0);
-        m6_control: in std_logic;
-        m1_control: in std_logic_vector(1 downto 0);
-        regfi_write_en: in std_logic;
-        data_memory_write_en: in std_logic;
-        
+        reg_a_data_sw: in std_logic_vector(15 downto 0);	--
+        dest_reg: in std_logic_vector(2 downto 0); --
+        alu_control: in std_logic_vector(2 downto 0);  --
+        carry_write_en: in std_logic; --
+        zero_flag_en: in std_logic; --
+        m5_control: in std_logic_vector(1 downto 0); ---
+        m6_control: in std_logic; ---
+        m1_control: in std_logic_vector(1 downto 0); --
+        m14_control: in std_logic;
+        opcode: in std_logic_vector(3 downto 0);
+        regfi_write_en: in std_logic; --
+        data_memory_write_en: in std_logic; --
         pr3_wr_en: in std_logic;
         clk: in std_logic;
-        pr3_out: out std_logic_vector(94 downto 0)); 
-            
 
+        pr3_out: out std_logic_vector(99 downto 0)); 
 end entity;
 
 architecture behave of pr3 is 
 
-signal reg_sig : std_logic_vector(94 downto 0);  -- sign_ext control
+signal reg_sig : std_logic_vector(99 downto 0);  -- sign_ext control
 
     
 begin 
-	process (clk)
+	process (clk,pr3_wr_en)
     begin
         if rising_edge(clk) then
             if pr3_wr_en = '1' then
-                reg_sig(15 downto 0) <= opr1;
-				reg_sig(31 downto 16) <= opr2;
-                reg_sig(47 downto 32) <= imm;
-                reg_sig(63 downto 48) <= pc_next;
-                reg_sig(79 downto 64) <= reg_a_data_sw;
-                reg_sig(82 downto 80) <= dest_reg;
-                reg_sig(85 downto 83) <= alu_control;
-                reg_sig(86 downto 86) <= carry_write_en;
-                reg_sig(87 downto 87) <= zero_flag_en;
-                reg_sig(89 downto 88) <= m5_control;
-                reg_sig(90 downto 90) <= m6_control;
-                reg_sig(92 downto 91) <= m1_control;
-                reg_sig(93 downto 93) <= regfi_write_en;
-                reg_sig(94 downto 94) <= data_memory_write_en;
+                reg_sig(15 downto 0) <= imm;
+				reg_sig(31 downto 16) <= opr1;
+                reg_sig(47 downto 32) <= opr2;
+                reg_sig(50 downto 48) <= dest_reg;
+                reg_sig(51) <= regfi_write_en;
+                reg_sig(52) <= data_memory_write_en;
+                reg_sig(53) <= carry_write_en;
+                reg_sig(54) <= zero_flag_en;
+                reg_sig(57 downto 55) <= alu_control;
+                reg_sig(59 downto 58) <= m1_control;
+                reg_sig(61 downto 60) <= m5_control;
+                reg_sig(62) <= m6_control;
+                reg_sig(63) <= m14_control;
+                reg_sig(67 downto 64) <= opcode;
+                reg_sig(83 downto 68) <= pc_next;
+                reg_sig(99 downto 84) <= reg_a_data_sw;
             end if;
         end if;
     end process;
