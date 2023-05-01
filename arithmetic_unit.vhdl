a1342a0400f6fc1180fb387b080101803e4812c0
sankethan <rsankethan9rathod@gmail.com>
2023-05-01 07:06:04 +0530

    added case statement

--- arithmetic_unit.vhdl
+++ arithmetic_unit.vhdl
@@ -1,6 +1,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
+entity full_adder is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c : in STD_LOGIC;
+           sum : out STD_LOGIC;
+           carry_out : out STD_LOGIC);
+end full_adder;
 
+architecture Behavioral of full_adder is
+begin
+    sum <= a xor b xor c;
+    carry_out <= (a and b) or (b and c) or (a and c);
+end Behavioral;
+
+library ieee;
+use ieee.std_logic_1164.all;
 entity arithmetic_unit is
 	port(
 		A, B: in std_logic_vector(15 downto 0);
@@ -21,8 +36,14 @@ signal carry_over , Out_sig: std_logic_vector(15 downto 0):=(others => '0');
 begin
     process(A,B,C2,Cy,carry_over,Out_sig)
     begin
-        if (C2='0') then
-
+        case C2 is
+            
+            when '1' =>
+            loop2: for j in 1 to 15 loop
+                Out_sig(j) <= a(j) NAND b(j) ;
+                
+            end loop loop2;
+            when others =>
             Out_sig(0) <= a(0) xor b(0) xor Cy;
             carry_over(0) <= (a(0) and b(0)) or (a(0) and Cy) or (b(0) and Cy);
     
@@ -32,24 +53,10 @@ begin
             end loop loop1;
             
             new_cy <= carry_over(15);
+        end case;
             
-        else 
             
-            loop2: for j in 1 to 15 loop
-                Out_sig(j) <= a(j) NAND b(j) ;
-                
-            end loop loop2;
-        end if;
-
-        
     end process;
-    process(Out_sig)
-    begin
-        if Out_sig ="0000000000000000" then
-            new_zero <= '1';
-        else 
-            new_zero <= '0';
-        end if;
-    end process;
+    
     C <= Out_sig;
 end architecture;
