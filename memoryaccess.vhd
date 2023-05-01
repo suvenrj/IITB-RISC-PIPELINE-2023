864a2f73346cf3792c2bbc109719e6b80ab12ce8
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-01 14:24:17 +0530

    Removed unnecessary inputs and outputs
    1) Removed instruction_in , instruction_out
    2) took opcode as input 
    3) added input ra_value which would be useful in SW
    4) input add_in(basically, ALU_OUT from exec stage) is address for LW instruction and data for other instructions

--- memoryaccess.vhd
+++ memoryaccess.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity memoryaccess is 
-    port (clock,write_en_in: in std_logic;instruction_in,data_in,add_in,data_imm_in:std_logic_vector(15 downto 0); instruction_out,data_out,data_imm_out : out std_logic_vector(15 downto 0));
+    port (clock,write_en_in: in std_logic; add_in, ra_value :in std_logic_vector(15 downto 0);data_out : out std_logic_vector(15 downto 0); opcode: in std_logic_vector(3 downto 0));
 end entity;
 
 architecture beh of memoryaccess is 
@@ -14,18 +14,17 @@ architecture beh of memoryaccess is
     signal temp_data: std_logic_vector(15 downto 0);
 
     begin
-        ram1: Memory port map(write_en_in,clock,add_in,data_in,temp_data);
+        ram1: Memory port map(write_en_in,clock, add_in ,ra_value, temp_data);
 
-        proc1 : process(temp_data)
+        proc1 : process(temp_data, add_in)     --in case of load, add_in acts as memory addres
         begin
-            if(instruction_In(15 downto 12) = "0100") then
+            if(opcode = "0100") then
             -- LM 0110
                 data_out <= temp_data;
             else
-                data_out <= data_in;
+                data_out <= add_in;   -- in cases apart from load, add_in is simply data
             end if;
         end process;
 
 
 end beh;
-
