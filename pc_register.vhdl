b74141142bacbf12c447548450539a7fada49349
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-02 11:18:02 +0530

    added reset

--- pc_register.vhdl
+++ pc_register.vhdl
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity pc_register is
-    port (pc_in: in std_logic_vector(15 downto 0); pc_out : out std_logic_vector(15 downto 0); clock: in std_logic; pc_write_en: in std_logic);
+    port (pc_in: in std_logic_vector(15 downto 0); pc_out : out std_logic_vector(15 downto 0); clock: in std_logic; pc_write_en, pc_reset: in std_logic);
 end entity;
 
 architecture behave of pc_register is 
@@ -11,14 +11,18 @@ architecture behave of pc_register is
         pc_out <= pc_val;
         proc: process(clock, pc_in, pc_write_en,pc_val)
             begin 
-                if (rising_edge(clock)) then
-                    if (pc_write_en='1') then
-                        pc_val <= pc_in;
+                if pc_reset = '0' then
+                    if (rising_edge(clock)) then
+                        if (pc_write_en='1') then
+                            pc_val <= pc_in;
+                        else
+                            pc_val <= pc_val;
+                        end if;
                     else
                         pc_val <= pc_val;
                     end if;
                 else
-                    pc_val <= pc_val;
+                    pc_val <= (others => '0');
                 end if;
         end process;
 end behave;
