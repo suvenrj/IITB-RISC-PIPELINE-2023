2e8ae0c356afedc02e99db1efbeb4ccdffab0ea5
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-02 11:05:24 +0530

    added reset

--- pr1.vhdl
+++ pr1.vhdl
@@ -5,6 +5,7 @@ entity pr1 is
     port(Instr : in std_logic_vector(15 downto 0); 
 			pr1_wr_en: in std_logic;
 			clk: in std_logic;
+	 		reset: in std_logic;
 			pr1_out: out std_logic_vector(15 downto 0)); 
 
 end entity;
@@ -15,13 +16,16 @@ signal reg_sig : std_logic_vector(15 downto 0);  -- sign_ext control
 
     
 begin 
-	process (clk,pr1_wr_en)
+process (clk,pr1_wr_en, reset)
     begin
-        if rising_edge(clk) then
-            if pr1_wr_en = '1' then
-                reg_sig <= instr;
-            end if;
-        end if;
-    end process;
-    pr1_out <= reg_sig;	 
+	if reset = '0' then
+		if rising_edge(clk) then
+		    if pr1_wr_en = '1' then
+			reg_sig <= instr;
+		    end if;
+		end if;
+	else
+		reg_sig <= (others => '0');
+end process;
+pr1_out <= reg_sig;	 
 end behave;
