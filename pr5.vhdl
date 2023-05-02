d846d895e35646d5a9dd88fca347a7dce04035a6
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-02 11:14:54 +0530

    added reset

--- pr5.vhdl
+++ pr5.vhdl
@@ -9,7 +9,7 @@ entity pr5 is
         dest_reg: in std_logic_vector(2 downto 0);	
         
         pr5_wr_en: in std_logic;
-		
+		reset: in std_logic;
 		pr5_out: out std_logic_vector(19 downto 0)
     ); 
 
@@ -23,13 +23,17 @@ signal reg_sig : std_logic_vector(19 downto 0);  -- sign_ext control
 begin 
 	process (clk,pr5_wr_en)
     begin
-        if rising_edge(clk) then
-            if pr5_wr_en = '1' then
-                reg_sig(15 downto 0) <= data_in;
-				reg_sig(18 downto 16) <= dest_reg;
-                reg_sig(19) <= rf_write_en;
-            end if;
-        end if;
+	if reset = '0' then
+		if rising_edge(clk) then
+		    if pr5_wr_en = '1' then
+			reg_sig(15 downto 0) <= data_in;
+					reg_sig(18 downto 16) <= dest_reg;
+			reg_sig(19) <= rf_write_en;
+		    end if;
+		end if;
+	else
+		reg_sig <= (others => '0');
+	end if;
     end process;
     pr5_out <= reg_sig;	 
 end behave;
