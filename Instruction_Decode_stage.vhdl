79f750238fcf52ee6d11ff5a5e2886d2b61352bc
suvenrj <99804277+suvenrj@users.noreply.github.com>
2023-05-02 11:38:39 +0530

    Update Instruction_Decode_stage.vhdl

--- Instruction_Decode_stage.vhdl
+++ Instruction_Decode_stage.vhdl
@@ -31,8 +31,9 @@ component pr2 is
          pc_next: in std_logic_vector(15 downto 0);				-- if Instruction at PC is at ID stage then PC_next is PC+2
 			pr2_wr_en: in std_logic;
 			clk: in std_logic;
-		
-			pr2_out: out std_logic_vector(55 downto 0)); 
+			pr2_out: out std_logic_vector(55 downto 0);
+			reset: in std_logic
+	); 
 
 end component;
 
